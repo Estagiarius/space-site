c6f679fc024a01c3cd3e20803735eff0d109e18f
Victor Hugo Garcia de Oliveira <victors.123@hotmail.com>
2018-05-09 22:16:22 -0300

    Começando a confecção do back-end

---
+++ cadastro.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+include "conexao.php";
+
+if(isset($_POST["CADASTRAR"])){
+
+    $user=$_POST["txtUsernameCad"];
+    $email=$_POST["txtEmailCad"];
+    $pass=$_POST["txtPasswordCad"];
+
+
+    $comando= "Insert into jogador(USERNAME_JOGADOR,SENHA_JOGADOR,EMAIL_JOGADOR) values ($user, $pass, $email)";
+
+    $resulta = mysqli_query($con,$comando);
+   
+    if ($resulta!=0) {
+        echo "<script> alert('inclusão ok');</script>";
+    }
+    else
+        echo "<script> alert('erro de inclusão');</script>";
+}
+
+$close = mysqli_close($con);
+
+?>
+
+<a href="login.html">Redirecionar</a>
