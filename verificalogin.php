3f6b43c35d910d3462b6c1da56c9572f246b9bf7
Victor Hugo Garcia de Oliveira <victors.123@hotmail.com>
2018-05-17 22:15:38 -0300

    Adicionando login

---
+++ verificalogin.php
@@ -0,0 +1,30 @@
+<?php
+
+	include "conexao.php";
+
+	SESSION_START();
+
+	$userLogin = $_POST["txtUsernameLog"];
+	$userPass = $_POST["txtPasswordLog"];
+
+	$resulta = mysqli_query($con, "select * from jogador where USERNAME_JOGADOR = '$userLogin' and SENHA_JOGADOR = '$userPass'");
+
+
+	if ($resulta->num_rows > 0){
+
+		$registro = mysqli_fetch_array($resulta);
+
+		$fechar = mysqli_close($con);
+
+		$_SESSION['userLogin'] = $userLogin;
+
+		$_SESSION['logado'] = 1;
+
+		header('Location: index.html');
+	}
+	else{
+		$fechar = mysqli_close($con);
+
+		echo "<script>alert('Login ou Senha Inválidos!') </script>";
+	}
+?>
